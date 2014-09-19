eb7777d2305280f3a993821af721ce8ad3b49990
Patrick Martin <patrick@orbotix.com>
2014-09-19 11:29:31 -0600

    First Post! Created a project, now getting around to figuring it out

---
+++ wear/src/main/java/com/orbotix/androidweardrive/AndroidWearDriveClient.java
@@ -0,0 +1,24 @@
+package com.orbotix.androidweardrive;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class AndroidWearDriveClient extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_wear_drive_client);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
