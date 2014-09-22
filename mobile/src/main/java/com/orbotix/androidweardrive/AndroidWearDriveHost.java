4e7d0041768c85206acbf28a0430a8497f40e38e
Patrick Martin <patrick@orbotix.com>
2014-09-22 09:13:42 -0600

    Some work to hook up the buttons and try to get some communication going between the watch and phone

--- mobile/src/main/java/com/orbotix/androidweardrive/AndroidWearDriveHost.java
+++ mobile/src/main/java/com/orbotix/androidweardrive/AndroidWearDriveHost.java
@@ -1,36 +1,123 @@
 package com.orbotix.androidweardrive;
 
 import android.app.Activity;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
-public class AndroidWearDriveHost extends Activity {
+import java.util.Collection;
+import java.util.HashSet;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_android_wear_drive_host);
-    }
+import static com.google.android.gms.common.api.GoogleApiClient.Builder;
+import static com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import static com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.android_wear_drive_host, menu);
-        return true;
-    }
+public class AndroidWearDriveHost extends Activity
+		implements ConnectionCallbacks, OnConnectionFailedListener {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private static final int REQUEST_RESOLVE_ERROR = 1001;
+	private static final String DIALOG_ERROR = "dialog_error";
+
+	private GoogleApiClient mGoogleApiClient;
+	private boolean mResolvingError = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_android_wear_drive_host);
+
+		mGoogleApiClient = new Builder(this).addApi(Wearable.API).build();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (!mResolvingError) {
+			mGoogleApiClient.connect();
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		mGoogleApiClient.disconnect();
+		super.onStop();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.android_wear_drive_host, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private Collection<String> getNodes() {
+		HashSet<String> results = new HashSet<String>();
+		NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+		for (Node node : nodes.getNodes()) {
+			results.add(node.getId());
+			Log.d("AWH", "node " + node.getId() + " found!");
+		}
+		return results;
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (mResolvingError) {
+			return;
+		}
+		else if (connectionResult.hasResolution()) {
+			try {
+				mResolvingError = true;
+				connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+			}
+			catch (IntentSender.SendIntentException e) {
+				// try again if we fail to resolve the error
+				mGoogleApiClient.connect();
+			}
+		}
+		else {
+
+		}
+	}
+
+	// generic error dialog code
+	private void showErrorDialog(int errorCode) {
+		ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+		Bundle args = new Bundle();
+		args.putInt(DIALOG_ERROR, errorCode);
+		dialogFragment.setArguments(args);
+		dialogFragment.show(getFragmentManager(), "errordialog");
+	}
 }
--- wear/src/main/java/com/orbotix/androidweardrive/AndroidWearDriveClient.java
+++ wear/src/main/java/com/orbotix/androidweardrive/AndroidWearDriveClient.java
@@ -3,11 +3,24 @@ package com.orbotix.androidweardrive;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AndroidWearDriveClient extends Activity {
 
-    private TextView mTextView;
+	private TextView mTextView;
+
+	private Button mBtnFL;
+	private Button mBtnF;
+	private Button mBtnFR;
+	private Button mBtnL;
+	private Button mBtnR;
+	private Button mBtnBL;
+	private Button mBtnB;
+	private Button mBtnBR;
+
+	private Button mBtnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,84 @@ public class AndroidWearDriveClient extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+
+				mBtnFL = (Button) stub.findViewById(R.id.btn_fl);
+				mBtnFL.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(315, 1.0f);
+					}
+				});
+
+				mBtnF = (Button) stub.findViewById(R.id.btn_f);
+				mBtnF.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(0, 1.0f);
+					}
+				});
+
+				mBtnFR = (Button) stub.findViewById(R.id.btn_fr);
+				mBtnFR.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(45, 1.0f);
+					}
+				});
+
+				mBtnL = (Button) stub.findViewById(R.id.btn_l);
+				mBtnL.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(270, 1.0f);
+					}
+				});
+
+				mBtnR = (Button) stub.findViewById(R.id.btn_r);
+				mBtnR.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(90, 1.0f);
+					}
+				});
+
+				mBtnBL = (Button) stub.findViewById(R.id.btn_bl);
+				mBtnBL.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(225, 1.0f);
+					}
+				});
+
+				mBtnB = (Button) stub.findViewById(R.id.btn_b);
+				mBtnB.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(180, 1.0f);
+					}
+				});
+
+				mBtnBR = (Button) stub.findViewById(R.id.btn_br);
+				mBtnBR.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(135, 1.0f);
+					}
+				});
+
+				mBtnStop = (Button) stub.findViewById(R.id.btn_stop);
+				mBtnStop.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Drive(0, 0f);
+					}
+				});
             }
         });
     }
+
+	private void Drive(int heading, float speed)
+	{
+
+	}
 }
